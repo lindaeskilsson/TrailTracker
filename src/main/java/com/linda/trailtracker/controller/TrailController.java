b2fb63d74718214eafc506e043ec162289eb832c
Linda Eskilsson <eskilsson.linda@hotmail.se>
2026-03-19 22:02:22 +0100

    feat: added mapping for create trail, create.html

--- src/main/java/com/linda/trailtracker/controller/TrailController.java
+++ src/main/java/com/linda/trailtracker/controller/TrailController.java
@@ -1,9 +1,12 @@
 package com.linda.trailtracker.controller;
 
+import com.linda.trailtracker.dto.CreateTrailDto;
 import com.linda.trailtracker.service.TrailService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,9 +19,23 @@ public class TrailController {
         this.trailService =trailService;
     }
 
+    //list of trails
     @GetMapping
     public String listTrails(Model model) {
         model.addAttribute("trails", trailService.getAllTrails());
-        return "list";
+        return "trails/list";
+    }
+
+    //create new trail
+    @GetMapping("/new")
+    public String showCreateForm(Model model) {
+        model.addAttribute("trail", new CreateTrailDto());
+        return "trails/create";
+    }
+
+    @PostMapping
+    public String createTrail(@ModelAttribute("trail") CreateTrailDto dto){
+        trailService.createTrail(dto);
+    return "redirect:/trails";
     }
 }
